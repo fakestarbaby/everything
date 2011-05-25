560c394a80ace22840d6f1c5b006c7d696225520
fakestarbaby <fakestarbaby@gmail.com>
2011-05-25 09:04:28 +0900

    twitterプラグインを追加。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   helper_method :current_user
+  helper_method :twitter
 
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def twitter
+    Twitter.configure do |config|
+      config.consumer_key = TWITTER_CONSUMER_KEY
+      config.consumer_secret = TWITTER_CONSUMER_SECRET
+      config.oauth_token = current_user.token
+      config.oauth_token_secret = current_user.secret
+    end
+    @twitter = Twitter::Client.new
   end
 end
